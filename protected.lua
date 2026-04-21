d1b971fa90142d2df30cf8319a826dc0dac178a9
edercleber0-droid <edercleber0@gmail.com>
2026-04-21 17:04:48 -0300

    Atualizar o protected.lua

--- protected.lua
+++ protected.lua
@@ -1,1 +1,5 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Frostzx-eternal/JUNINHUB/refs/heads/main/JUNINHUB"))()
+-- SCRIPT PROTEGIDO
+
+print("SCRIPT LIBERADO ✔")
+
+--loadstring(game:HttpGet("https://raw.githubusercontent.com/Frostzx-eternal/JUNINHUB/refs/heads/main/JUNINHUB"))()
